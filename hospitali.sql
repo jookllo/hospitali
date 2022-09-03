f15d23fa7f972dd43b77c7607e974634691ce4db
evans45 <muangeevans77@gmail.com>
2022-09-03 13:06:44 +0300

    Add files via upload

---
+++ hospitali.sql
@@ -0,0 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Sep 03, 2022 at 10:06 AM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hospitali`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patients`
+--
+
+DROP TABLE IF EXISTS `patients`;
+CREATE TABLE IF NOT EXISTS `patients` (
+  `patient_id` varchar(20) NOT NULL,
+  `patient_name` varchar(50) NOT NULL,
+  `patient_dob` date DEFAULT NULL,
+  `insurance_scheme` varchar(30) NOT NULL,
+  `policy_number` varchar(30) NOT NULL,
+  `reg_date` date DEFAULT NULL,
+  PRIMARY KEY (`patient_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `password`) VALUES
+('evans', '12345678');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vitals`
+--
+
+DROP TABLE IF EXISTS `vitals`;
+CREATE TABLE IF NOT EXISTS `vitals` (
+  `patient_id` varchar(20) NOT NULL,
+  `temperature` float NOT NULL,
+  `height` float NOT NULL,
+  `weight` float NOT NULL,
+  `blood_presssure` varchar(10) NOT NULL,
+  `Doctor` varchar(30) NOT NULL,
+  PRIMARY KEY (`patient_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
